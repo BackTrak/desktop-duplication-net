0019b21daeb989a526c908cd7af7fa1bdfb75eef
Nick Pirocanac <nick@zaphop.com>
2020-09-25 15:37:56 -0500

    Upgraded to latest SharpDX version and got it running under VS2019.

---
+++ DesktopDuplication/SharpDXRectangleExtensions.cs
@@ -0,0 +1,18 @@
+﻿
+using Rectangle = SharpDX.Mathematics.Interop.RawRectangle;
+
+namespace DesktopDuplication
+{
+    public static class SharpDXRectangleExtensions
+    {
+        public static int Width(this Rectangle rectangle)
+        {
+            return rectangle.Right - rectangle.Left;
+        }
+
+        public static int Height(this Rectangle rectangle)
+        {
+            return rectangle.Bottom - rectangle.Top;
+        }
+    }
+}
